69fa41cc3763b6da40bb4b8168a9d1a6e1f36ffe
Vanh@FutureWei <vphomsav@futurewei.com>
2022-06-28 18:11:28 -0700

    improve on the code to do better detection

--- pkg_ch.sh
+++ pkg_ch.sh
@@ -6,21 +6,71 @@ check_bashVersion(){
 	echo "your bash version is: $bash_version"
 
 	bash_path=$(which bash)
+	echo " "
 	echo "your bash path is: $bash_path"
 	
 }
 
+install_npm(){
+
+	npm_ver=$(npm -v)
+	echo "checking your npm vesion"
+	echo "your npm version is: $npm_ver"
+	sudo apt install npm
+}
+
+download_node(){
+	echo "downloading node.js from the internet..."
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+	#sudo apt install nodejs
+}
+
+install_node(){
+
+	echo "Installing node.js begun..."
+	download_node
+	install_npm
+
+
+}
+
+
+install_bubblewrap(){
+	echo "Installing bubblewrap command line"
+	source npm i -g @bubblewrap/cli
+	# source npm install -g npm@8.13.1
+	make_android_proj
+}
+
+make_android_proj(){
+	echo "create Android project..."
+	bubblewrap init --manifest https://my-twa.com/manifest.json
+}
+
+
 check_nodeVersion(){
 	echo "checking your node.js version"
-	node_version=`node --version |awk 'NR==1{gsub("v",""); print $1 }'`
+	# node_version=`node --version |awk 'NR==1{gsub("v",""); print $1 }'`
+
+	node_version=$(node --version)
+	echo -n "the value is: ${node_version:- "no node.js found on this system"}"
 	# export NODE_VERSION
 	# echo $NODE_VERSION
+	echo " "
 
 	echo "your current node version is: $node_version"
-	if [[ -z "$node_version" ]];then
+	if [ -z "$node_version" ]; then 
+		echo $?
 		echo "you don't have node install"
 		echo "I'm going to install node "
+		# echo " position parameter 0 is: $0 "
+		# echo " position parameter 1 is: $1 "
+		# echo " position parameter s is: $* "
+		# echo " position parameter s1 is: $# "
+		
+				
 		install_node
+	
 
 	else
 		echo "your Node.js is: $node_version"
@@ -30,52 +80,22 @@ check_nodeVersion(){
 		# cut -c 3 <<< $NODE_VERSION
 		# echo "minor value is $minor"
 
-		maj=${node_version:0:2}
+		maj=${node_version:1:2}
 		echo "major: $maj"
-		minor=${node_version:3:1}
+		minor=${node_version:4:1}
 		echo "minor: $minor"
 
 		if [[($maj -lt 12)]]; then
 			echo "Your node.js version too old Please upgrade to the latest version"
 		elif [[($minor -gt 0)]]; then
 			echo "You are good to go"
-			
+			install_bubblewrap
 
 		fi
 
 	fi
 
 }
-install_npm(){
-
-	npm_ver=$(npm -v)
-		echo "checking your npm vesion"
-		echo "your npm version is: $npm_ver"
-}
-
-download(){
-	apt -v
-}
-
-install_node(){
-
-			echo "download node.js from the internet..."
-			download
-
-			echo "Installing Bubblewrap on to your system"
-			# npm i -g @bubblewrap/cli
-
-			echo "bubblewrap: "
-			echo "JDK version 8 lower or higher version is not compatible"
-			echo "checking for correct path location"
-			# bubblewrap doctor
-
-			echo " It's now going to generate an Android project from an existing Web Manifest"
-			# bubblewrap init --manifest https://my-twa.com/manifest.json
-			install_npm
-
-}
-
 
 
 ################################################################################################
